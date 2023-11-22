b02bb491c6b6708351e298cce5ed113bf9004412
joshuashea <joshshea44@gmail.com>
2023-11-22 09:32:41 -0800

    fixed create character navigating to game page after creation

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'https://questforgebackend.onrender.com'; // Replace with your server URL
+// const API_URL = 'https://questforgebackend.onrender.com'; // Replace with your server URL
+const API_URL = 'http://localhost:3001';
 
 export const createCharacter = async (characterData) => {
   try {
-    const response = await axios.post(
-      `${API_URL}/character/createcharacter`,
-      characterData
-    );
+    const response = await axios.post(`${API_URL}/characters`, characterData);
     return response.data;
   } catch (error) {
     console.error('Error creating character:', error);
