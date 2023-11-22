ad1dca0127e0c2495e2fdf6b2dc62c8aaec8860b
SamaadTurner <samaad.turner@gmail.com>
2023-11-21 16:35:32 -0800

    deleted id field in form

--- src/screens/CreateCharacterScreen.js
+++ src/screens/CreateCharacterScreen.js
@@ -138,24 +138,6 @@ const CreateCharacterScreen = ({ navigation }) => {
 
       <RNPickerSelect
         style={pickerSelectStyles}
-        placeholder={{ label: 'Enter ID', value: '' }}
-        onValueChange={(itemValue) => setID(itemValue)}
-        items={[
-          { label: '1', value: '1' },
-          { label: '2', value: '2' },
-          { label: '3', value: '3' },
-          { label: '4', value: '4' },
-          { label: '5', value: '5' },
-          { label: '6', value: '6' },
-          { label: '7', value: '7' },
-          { label: '8', value: '8' },
-          { label: '9', value: '9' },
-          { label: '10', value: '10' },
-        ]}
-      />
-
-      <RNPickerSelect
-        style={pickerSelectStyles}
         placeholder={{ label: 'Select Age', value: '' }}
         onValueChange={(itemValue) => setAge(itemValue)}
         items={[...Array(ageLimit + 1).keys()].map((ageNumber) => ({
